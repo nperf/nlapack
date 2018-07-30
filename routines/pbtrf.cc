dcbe8cff257fb09e989c1bf0a1ec2e42607202e8
Mateo Gianolio <gianoliomateo@gmail.com>
2018-07-30 23:13:37 +0200

    implement *pbtrf with lapacke bindings

--- routines/pbtrf.cc
+++ routines/pbtrf.cc
@@ -1,28 +1,29 @@
+#include "lapacke.h"
 #include "routines.h"
 
 void dpbtrf(const v8::FunctionCallbackInfo<v8::Value>& info) {
-	int uplo = info[0]->Uint32Value();
-	int n = info[1]->Uint32Value();
-	int kd = info[2]->Uint32Value();
-	double *ab = reinterpret_cast<double*>(info[3].As<v8::Float64Array>()->Buffer()->GetContents().Data());
-	int i;
-	FORTRAN_DOUBLE_ORDER(kd + 1, n, ab);
-	dpbtrf_(&uplo, &n, &kd, ab, &i);
-	FORTRAN_DOUBLE_ORDER(kd + 1, n, ab);
+	char uplo = info[0]->Uint32Value();
+	lapack_int n = info[1]->Uint32Value();
+	lapack_int kd = info[2]->Uint32Value();
+	double *ab = reinterpret_cast<double*>(GET_CONTENTS(info[3].As<v8::Float64Array>()));
+	lapack_int ldab = info[4]->Uint32Value();
+
+	lapack_int i = LAPACKE_dpbtrf(LAPACK_ROW_MAJOR, uplo, n, kd, ab, ldab);
+
 	info.GetReturnValue().Set(
 		v8::Number::New(info.GetIsolate(), i)
 	);
 }
 
 void spbtrf(const v8::FunctionCallbackInfo<v8::Value>& info) {
-	int uplo = info[0]->Uint32Value();
-	int n = info[1]->Uint32Value();
-	int kd = info[2]->Uint32Value();
-	float *ab = reinterpret_cast<float*>(info[3].As<v8::Float32Array>()->Buffer()->GetContents().Data());
-	int i;
-	FORTRAN_SINGLE_ORDER(kd + 1, n, ab);
-	spbtrf_(&uplo, &n, &kd, ab, &i);
-	FORTRAN_SINGLE_ORDER(kd + 1, n, ab);
+	char uplo = info[0]->Uint32Value();
+	lapack_int n = info[1]->Uint32Value();
+	lapack_int kd = info[2]->Uint32Value();
+	float *ab = reinterpret_cast<float*>(GET_CONTENTS(info[3].As<v8::Float32Array>()));
+	lapack_int ldab = info[4]->Uint32Value();
+
+	lapack_int i = LAPACKE_spbtrf(LAPACK_ROW_MAJOR, uplo, n, kd, ab, ldab);
+
 	info.GetReturnValue().Set(
 		v8::Number::New(info.GetIsolate(), i)
 	);
