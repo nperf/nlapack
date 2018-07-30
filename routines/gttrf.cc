92420a63cd6532b3557a82cea5d42fbda8a55841
Mateo Gianolio <gianoliomateo@gmail.com>
2018-07-30 23:13:20 +0200

    implement *gttrf with lapacke bindings

--- routines/gttrf.cc
+++ routines/gttrf.cc
@@ -1,32 +1,33 @@
+#include "lapacke.h"
 #include "routines.h"
 
 void dgttrf(const v8::FunctionCallbackInfo<v8::Value>& info) {
-	int n = info[0]->Uint32Value();
-	double *dl = reinterpret_cast<double*>(info[1].As<v8::Float64Array>()->Buffer()->GetContents().Data());
-	double *d = reinterpret_cast<double*>(info[2].As<v8::Float64Array>()->Buffer()->GetContents().Data());
-	double *du = reinterpret_cast<double*>(info[3].As<v8::Float64Array>()->Buffer()->GetContents().Data());
-	double *du2 = reinterpret_cast<double*>(info[4].As<v8::Float64Array>()->Buffer()->GetContents().Data());
-	int *ipiv = reinterpret_cast<int*>(info[5].As<v8::Int32Array>()->Buffer()->GetContents().Data());
-	int i;
-	IPIV_FORTRAN(n, ipiv);
-	dgttrf_(&n, dl, d, du, du2, ipiv, &i);
+	lapack_int n = info[0]->Uint32Value();
+	double *dl = reinterpret_cast<double*>(GET_CONTENTS(info[1].As<v8::Float64Array>()));
+	double *d = reinterpret_cast<double*>(GET_CONTENTS(info[2].As<v8::Float64Array>()));
+	double *du = reinterpret_cast<double*>(GET_CONTENTS(info[3].As<v8::Float64Array>()));
+	double *du2 = reinterpret_cast<double*>(GET_CONTENTS(info[4].As<v8::Float64Array>()));
+	lapack_int *ipiv = reinterpret_cast<int*>(GET_CONTENTS(info[5].As<v8::Int32Array>()));
+
+	lapack_int i = LAPACKE_dgttrf(n, dl, d, du, du2, ipiv);
 	IPIV_C(n, ipiv);
+
 	info.GetReturnValue().Set(
 		v8::Number::New(info.GetIsolate(), i)
 	);
 }
 
 void sgttrf(const v8::FunctionCallbackInfo<v8::Value>& info) {
-	int n = info[0]->Uint32Value();
-	float *dl = reinterpret_cast<float*>(info[1].As<v8::Float32Array>()->Buffer()->GetContents().Data());
-	float *d = reinterpret_cast<float*>(info[2].As<v8::Float32Array>()->Buffer()->GetContents().Data());
-	float *du = reinterpret_cast<float*>(info[3].As<v8::Float32Array>()->Buffer()->GetContents().Data());
-	float *du2 = reinterpret_cast<float*>(info[4].As<v8::Float32Array>()->Buffer()->GetContents().Data());
-	int *ipiv = reinterpret_cast<int*>(info[5].As<v8::Int32Array>()->Buffer()->GetContents().Data());
-	int i;
-	IPIV_FORTRAN(n, ipiv);
-	sgttrf_(&n, dl, d, du, du2, ipiv, &i);
+	lapack_int n = info[0]->Uint32Value();
+	float *dl = reinterpret_cast<float*>(GET_CONTENTS(info[1].As<v8::Float32Array>()));
+	float *d = reinterpret_cast<float*>(GET_CONTENTS(info[2].As<v8::Float32Array>()));
+	float *du = reinterpret_cast<float*>(GET_CONTENTS(info[3].As<v8::Float32Array>()));
+	float *du2 = reinterpret_cast<float*>(GET_CONTENTS(info[4].As<v8::Float32Array>()));
+	lapack_int *ipiv = reinterpret_cast<int*>(GET_CONTENTS(info[5].As<v8::Int32Array>()));
+
+	lapack_int i = LAPACKE_sgttrf(n, dl, d, du, du2, ipiv);
 	IPIV_C(n, ipiv);
+
 	info.GetReturnValue().Set(
 		v8::Number::New(info.GetIsolate(), i)
 	);
