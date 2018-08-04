0a85c50dceb95cd25d0e7e76517d9b650bfc876d
Mateo Gianolio <gianoliomateo@gmail.com>
2018-08-04 16:58:35 +0200

    bugfixes

--- routines/lineq/refine/gbrfs.cc
+++ routines/lineq/refine/gbrfs.cc
@@ -32,7 +32,7 @@ void sgbrfs(const v8::FunctionCallbackInfo<v8::Value>& info) {
  	lapack_int ku = info[3]->Uint32Value();
 	lapack_int nrhs = info[4]->Uint32Value();
 	float *ab = reinterpret_cast<float*>(GET_CONTENTS(info[5].As<v8::Float32Array>()));
-	lapack_intb lda = info[6]->Uint32Value();
+	lapack_int ldab = info[6]->Uint32Value();
 	float *afb = reinterpret_cast<float*>(GET_CONTENTS(info[7].As<v8::Float32Array>()));
 	lapack_int ldafb = info[8]->Uint32Value();
 	lapack_int *ipiv = reinterpret_cast<int*>(GET_CONTENTS(info[9].As<v8::Int32Array>()));
